1af8d4acfff3e368bfbddfc10714b1cadd14d37c
Maximilian Oertel <info@maximilianoertel.de>
2021-01-17 18:25:27 -0500

    Implemented captive portal page [ref #23]

--- frontend/apps/interface/src/components/Header/Header.vue
+++ frontend/apps/interface/src/components/Header/Header.vue
@@ -3,7 +3,7 @@
         <div class="el-header__content">
             <div class="el-container is-vertical">
                 <div class="el-header__nav">
-                    <el-nav type="light"></el-nav>
+                    <el-nav type="light" :mode="navMode ? navMode : ''"></el-nav>
                 </div>
 
                 <div class="el-header__inner" v-if="this.title">
@@ -36,7 +36,6 @@
 
 <script>
 import ElNav from '@/components/Nav/Nav'
-
 import ElIcon from '@/components/Icons/Icon'
 
 import ShapeOne from '@/assets/shapes/blob-shape-01.svg'
@@ -45,12 +44,11 @@ import ShapeTwo from '@/assets/shapes/blob-shape-02.svg'
 export default {
     name: 'Header',
     props: {
-        type: String,
-        size: String,
         title: String,
         back: String,
         showBack: Boolean,
-        breadcrumbs: Array
+        breadcrumbs: Array,
+        navMode: String
     },
     components: {
         ElNav,
@@ -59,19 +57,23 @@ export default {
         ShapeTwo
     },
     computed: {
+        /**
+         * Builds the additional classes based on the type & size props
+         */
         classes: function () {
             let classes = ''
 
-            // handle type
+            // Handle type prop
             if (this.type) {
                 classes += 'el-header--' + this.type
             }
 
-            // handle size
+            // Handle size prop
             if (this.size) {
                 classes += ' el-header--' + this.size
             }
 
+            // Return additional classes string
             return classes
         }
     }
---
+++ frontend/apps/interface/src/components/Message/Message.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="el-message">
+        <div class="el-container is-vertical">
+            <div class="el-message__inner">
+                <div class="el-message__icon" v-if="this.icon">
+                    <el-icon
+                        :name="this.icon"
+                        size="lg">
+                    </el-icon>
+                </div>
+
+                <div class="el-message__title">
+                    <slot name="title"></slot>
+                </div>
+
+                <div class="el-message__content">
+                    <slot name="content"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ElIcon from '@/components/Icons/Icon'
+
+export default {
+    name: 'Message',
+    props: {
+        icon: String
+    },
+    components: {
+        ElIcon
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import 'Message.scss';
+</style>
