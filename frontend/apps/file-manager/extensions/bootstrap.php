1e03a1ce5001b73b010148e89cc724f913654020
Maximilian Oertel <info@maximilianoertel.de>
2021-04-04 18:26:42 -0400

    Removed dotenv support

--- frontend/apps/file-manager/extensions/bootstrap.php
+++ frontend/apps/file-manager/extensions/bootstrap.php
@@ -1,60 +1,48 @@
 <?php
 namespace IFM_Extensions;
-use Dotenv;
 
 /**
  * IFM Extensions Loader
  * 
- * Load all custom extensions for the IFM filemanager
+ * Load all custom extensions for the IFM Filemanager
  * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
 
 class IFM_Extensions {
     function __construct() {
-        // define some base constants
+        // Set environment vars
+        $is_dev_env     = (getenv('ENV') === 'development');
+        $host_dev_env   = '0.0.0.0:9090';
+        $host_prod_env  = $_SERVER['HTTP_HOST'];
+        
+        // Prepare settings
         define('ROOT_PATH', dirname(dirname(__FILE__)));
         define('EXTENSIONS_PATH', ROOT_PATH . '/extensions');
+        define('LB_API_BASE_URL', $is_dev_env ? $host_dev_env : $host_prod_env);
 
-        // load composer
+        // Load composer
         require_once ROOT_PATH . '/vendor/autoload.php';
 
-        // load all custom extensions
-        $this->load_dotenv();
+        // Load custom extensions
         $this->load_extensions();
-    }
-
-
-
-    /**
-     * load_dotenv
-     * 
-     * Loads the .env confile file into the filemanager to provide environment
-     * variable support for IFM and the extensions
-    */
-
-    private function load_dotenv () {
-        // load dotenv config
-        $dotenv = Dotenv\Dotenv::createImmutable(ROOT_PATH);
-        $dotenv->load();
     }
 
 
     
     /**
-     * load_extensions
+     * Load Extensions
      * 
-     * Loads all custom extensions for the IFM filemanager such as auth, etc.
+     * Loads all custom extensions for the IFM Filemanager such as auth, etc.
     */
 
     private function load_extensions () {
-        // auth
+        // Auth
         require_once EXTENSIONS_PATH . '/auth.php';
         $extension_auth = new \IFM_Extensions\JWTAuth();
 
-        // lang
+        // Lang
         require_once EXTENSIONS_PATH . '/lang.php';
         $extension_lang = new \IFM_Extensions\Lang();
     }
 }
-
 
 new IFM_Extensions();
