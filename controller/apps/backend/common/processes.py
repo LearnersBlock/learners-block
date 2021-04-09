66e0ed9787fd8dee117096120b928fad6b9ec075
maggie0002 <arran.magee@icloud.com>
2021-04-08 19:56:40 -0700

    Add download as RSync fallback

--- controller/apps/backend/common/processes.py
+++ controller/apps/backend/common/processes.py
@@ -7,14 +7,14 @@ import socket
 import subprocess
 import time
 
-# Set defalut global variable for terminating RSync
+
+# Set global variables
+download_log = ''
+download_terminate = 0
 rsync_log = ''
 rsync_status = {
-                "progress": "0%",
-                "complete": False,
-                "transferred": 0,
-                "speed": 0
-                }
+    "progress": 'loading'
+    }
 
 
 def check_space():
@@ -138,6 +138,43 @@ def database_recover():
         print("Failed to delete the database file. May be missing.")
 
 
+def download_start(url: str):
+    global download_log
+    global download_terminate
+    download_log = 0
+    downloaded_percentage = 0
+    try:
+        resp = requests.get(url, stream=True)
+    except Exception:
+        download_log = "error"
+        return
+    total = int(resp.headers.get('content-length', 0))
+    with open(os.path.realpath('.') + '/storage/library/' + url.split('/')[-1], 'wb') as file:
+        for data in resp.iter_content(chunk_size=1024):
+            if download_terminate == 1:
+                download_terminate = 0
+                return {"progress": "termianted"}
+            size = file.write(data)
+            downloaded_percentage += size
+            download_log = downloaded_percentage/total*100/100
+
+    print("Download complete")
+
+
+def download_get_status():
+    global download_log
+    if check_space() is True:
+        download_terminate()
+        return {"progress": "space-error"}
+    return {"progress": download_log}
+
+
+def download_terminate():
+    # Terminate RSync upon user request
+    global download_terminate
+    download_terminate = 1
+
+
 def human_size(nbytes):
     # Convert system file sizes to human readable
     suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
@@ -149,10 +186,13 @@ def human_size(nbytes):
     return '%s %s' % (f, suffixes[i])
 
 
-def rsync_download(rsync_url):
-
+def rsync_start(rsync_url):
     global rsync_proc
-    # Download requested via RSync
+    global rsync_status
+    rsync_status = {
+            "progress": 'loading'
+            }
+
     try:
         if rsync_proc.poll() is None:
             return "running"
@@ -170,6 +210,12 @@ def rsync_download(rsync_url):
         start_new_session=True
     )
 
+    time.sleep(4)
+
+    if rsync_proc.poll() is not None and rsync_proc.returncode != 0:
+        rsync_terminate(outcome='error')
+        return 1
+
     try:
         for line in rsync_proc.stdout:
             global rsync_log
@@ -177,7 +223,7 @@ def rsync_download(rsync_url):
     except Exception:
         print("RSync Proc terminated. Ending logging")
 
-    rsync_terminate()
+    rsync_terminate(outcome="complete")
 
     return 0
 
@@ -187,39 +233,38 @@ def rsync_get_status():
     global rsync_log
 
     if check_space() is True:
-        rsync_terminate()
+        rsync_terminate(outcome='space-error')
         return {"progress": "space-error"}
 
-    if rsync_status['complete'] is True:
-        rsync_status['complete'] = False
-        return {"progress": "1%",
-                "complete": True}
+    if rsync_status['progress'] == "error":
+        return {"progress": rsync_status['progress']}
+
+    if rsync_status['progress'] == "complete":
+        return {"progress": rsync_status['progress']}
 
     # Split log lines
     each_line = rsync_log.split()
 
     if each_line:
+        print(each_line[1][:-1])
         json_output = {
+            "progress": int(each_line[1][:-1])/100,
             "transferred": each_line[0],
-            "progress": each_line[1],
             "speed": each_line[2],
-            "remaining_time": each_line[3],
-            "comparing_files": False,
-            "complete": False
+            "remaining_time": each_line[3]
             }
     else:
         json_output = {
-            "progress": '0%',
-            "comparing_files": True,
-            "complete": False,
+            "progress": 'checking-files',
             "transferred": 0,
-            "speed": 0
+            "speed": 0,
+            "remaining_time": 0
             }
 
     return json_output
 
 
-def rsync_terminate():
+def rsync_terminate(outcome='complete'):
     # Terminate RSync upon user request
     try:
         global rsync_proc
@@ -235,10 +280,11 @@ def rsync_terminate():
         except Exception:
             print("Could not kill")
 
+    global rsync_log
+    rsync_log = ''
+
     global rsync_status
     rsync_status = {
-                "progress": "1%",
-                "complete": True,
-                "terminated": 'yes'
+                "progress": outcome
                 }
     return "Rsync terminated"
