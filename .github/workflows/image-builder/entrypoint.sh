dbef15f6ee12af1d43a59f51c0a9822964e3ba49
maggie0002 <arran.magee@icloud.com>
2021-07-28 17:30:41 -0700

    Add synchronous Balena pre-loads

--- .github/workflows/image-builder/entrypoint.sh
+++ .github/workflows/image-builder/entrypoint.sh
@@ -21,24 +21,26 @@ do
   balena push $line
 done
 
-# Make required build directory
+# Pre-load images
 for env in $GITHUB_WORKSPACE/.github/workflows/image-builder/env-files/$1/*.env; do
 
   # Export env variables from file
-  export $(grep -v '^#' "$env" | xargs -d '\n')
+  (export $(grep -v '^#' "$env" | xargs -d '\n');
 
-  imageFile="$GITHUB_WORKSPACE/.github/workflows/image-builder/Learners-Block-$device_name-$RELEASE_VERSION.img"
+  imageFile="$GITHUB_WORKSPACE/.github/workflows/image-builder/Learners-Block-$device_name-$RELEASE_VERSION.img";
 
   # Fetch required image file
-  balena os download $type -o $imageFile
+  balena os download $type -o $imageFile; 
 
   # Preload files into image file
-  balena preload $imageFile --app $app --commit latest --debug
+  balena preload $imageFile --app $app --commit latest --debug; 
 
   # Inject the config file to the image
-  balena os configure $imageFile --config-app-update-poll-interval $appUpdatePollInterval --config-network=ethernet --application $app --debug
+  balena os configure $imageFile --config-app-update-poll-interval $appUpdatePollInterval --config-network=ethernet --application $app --debug; 
 
   # Zip the image file ready for adding to release assets
-  zip -j $GITHUB_WORKSPACE/Learners-Block-$device_name-$RELEASE_VERSION.zip $imageFile
+  zip -j $GITHUB_WORKSPACE/Learners-Block-$device_name-$RELEASE_VERSION.zip $imageFile) &
 
 done
+
+wait || exit 1
