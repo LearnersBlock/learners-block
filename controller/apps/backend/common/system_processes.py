e3abab7cc8afda012546fd822d9b5652cf88ddea
maggie0002 <arran.magee@icloud.com>
2021-08-16 11:09:34 -0700

    Increase check_internet timeout

--- controller/apps/backend/common/system_processes.py
+++ controller/apps/backend/common/system_processes.py
@@ -7,7 +7,7 @@ import socket
 import time
 
 
-def check_internet(host="8.8.8.8", port=53, timeout=3):
+def check_internet(host="8.8.8.8", port=53, timeout=6):
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
