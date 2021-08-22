8ae9048cf7cd17e50144bd18edc10df7e0e4794b
maggie0002 <arran.magee@icloud.com>
2021-08-22 16:01:58 -0700

    Update webpack-dev-server packages

--- frontend/apps/interface/quasar.conf.js
+++ frontend/apps/interface/quasar.conf.js
@@ -86,9 +86,11 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
+      client: {
+        webSocketURL: 'auto://0.0.0.0:0/ws'
+      },
       https: false,
       host: '0.0.0.0',
-      public: '0.0.0.0:8081',
       port: 8081,
       open: true // opens browser window automatically
     },
