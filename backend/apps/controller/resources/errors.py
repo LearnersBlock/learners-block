22a8b3a7bcb1ffeb5c40d9bc2239714f17af5255
maggie0002 <arran.magee@icloud.com>
2021-09-17 17:43:57 -0700

    Remove error messages from non-errors

--- backend/apps/controller/resources/errors.py
+++ backend/apps/controller/resources/errors.py
@@ -1,7 +1,11 @@
 # Message formatter for printing standard messages from functions
-def print_message(function='Unspecified', message='None', ex='Not Specified'):
-    print('function: ' + str(function) + " - message: " +
-          str(message) + " - error: " + str(ex), flush=True)
+def print_message(function='Unspecified', message='None', ex=None):
+    if ex is None:
+        print('function: ' + str(function) + " - message: " +
+              str(message), flush=True)
+    else:
+        print('function: ' + str(function) + " - message: " +
+              str(message) + " - error: " + str(ex), flush=True)
 
 
 # Custom error messages for Flask-RESTful to return
