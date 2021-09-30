25d4a1d4a1e9f7afa5ae6fc6e9f791c0ba155e57
maggie0002 <arran.magee@icloud.com>
2021-09-29 18:52:58 -0700

    Re-format error messages

--- backend/apps/controller/common/errors.py
+++ backend/apps/controller/common/errors.py
@@ -1,11 +1,11 @@
 # Message formatter for printing standard messages from functions
 def print_message(function='Unspecified', message='None', ex=None):
     if ex is None:
-        print('function: ' + str(function) + " - message: " +
+        print('function: ' + str(function) + "; message: " +
               str(message), flush=True)
     else:
-        print('function: ' + str(function) + " - message: " +
-              str(message) + " - error: " + str(ex), flush=True)
+        print('function: ' + str(function) + "; message: " +
+              str(message) + "; error: " + str(ex), flush=True)
 
 
 # Custom error messages for Flask-RESTful to return
