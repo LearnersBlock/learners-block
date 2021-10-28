898f3d099e748324cf47ef876dea39f0805818f9
maggie0002 <arran.magee@icloud.com>
2021-10-28 12:51:24 +0100

    Override default Axios on inital API check

--- frontend/apps/interface/src/App.vue
+++ frontend/apps/interface/src/App.vue
@@ -12,14 +12,14 @@
 </template>
 
 <script lang="ts">
-import Axios from 'axios'
+import { AxiosOverride } from 'src/boot/axios'
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   setup () {
     const apiIsUp = ref<boolean>(true)
 
-    Axios.get('http://' + window.location.hostname + ':9090')
+    AxiosOverride.get('http://' + window.location.hostname + ':9090', { timeout: 4000 })
       .catch(function (error) {
         apiIsUp.value = false
         console.log(error)
