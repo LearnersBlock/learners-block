79fe7dca77da2bed44ce6bad7ffe228623961490
maggie0002 <arran.magee@icloud.com>
2021-10-28 11:43:50 +0100

    Add NTP check to Docker container launches

--- backend/apps/controller/common/docker.py
+++ backend/apps/controller/common/docker.py
@@ -1,33 +1,62 @@
 import docker
+import ntplib
 import os
 from common.errors import logger
 from flask_restful import abort
 
-try:
-    # Import relevant UNIX path
-    if os.environ['FLASK_ENV'].lower() == "production":
-        client = \
-            docker.DockerClient(base_url='unix://var/run/balena-engine.sock',
-                                tls=False)
-    else:
-        client = docker.DockerClient(base_url='unix://var/run/docker.sock',
-                                     tls=False)
-except Exception:
-    logger.error('Docker socket is unavailable.')
-    pass
+
+# Import relevant UNIX path
+if os.environ['FLASK_ENV'].lower() == "production":
+    client_path = 'unix://var/run/balena-engine.sock'
+else:
+    client_path = 'unix://var/run/docker.sock'
+
+# Initiate NTP Library
+ntp = ntplib.NTPClient()
+
+
+# Check time has been synced before request
+def docker_ntp_check(func):
+    def inner(*args, **kwargs):
+        try:
+            # Check if the system clock is in sync otherwise Docker Hub
+            # certificates create an error
+            time_offset = ntp.request('time.cloudflare.com', version=3).offset
+            logger.info(f"Time offset is: {time_offset}")
+            if time_offset > 3600 or time_offset < -3600:
+                logger.debug('Not in sync with NTP server.')
+                abort(502,
+                      status=502,
+                      message='System is still loading. Try again later.')
+        except Exception:
+            logger.exception('Failed to check time with NTP server.')
+            # In event of connection error, allowing Docker to try anyway
+            pass
+
+        return func(*args, **kwargs)
+
+    return inner
 
 
 # Check Docker is available before sending the request
 def docker_ping(func):
     def inner(*args, **kwargs):
         try:
+            global client
+            # If the Docker client is not yet initiated
+            if 'client' not in globals():
+                client = docker.DockerClient(base_url=client_path,
+                                             tls=False)
+            # Check the docker client is available
             client.ping()
-            return func(*args, **kwargs)
-        except docker.errors.APIError:
+        except Exception:
             logger.exception('Docker UNIX socket is unreachable.')
             abort(502,
                   status=502,
-                  message='Docker service is unreachable.',)
+                  message='Docker service is unreachable.')
+
+        return func(*args, **kwargs)
+
     return inner
 
 
@@ -62,6 +91,7 @@ class docker_py():
         return {"message": "done", "status_code": 200}
 
     @docker_ping
+    @docker_ntp_check
     def pull(env_vars, image, name, ports, volumes, network, detach=True):
         try:
             container = client.containers.get(name)
@@ -102,6 +132,7 @@ class docker_py():
         return {"message": "done", "status_code": 200}
 
     @docker_ping
+    @docker_ntp_check
     def run(image, name, ports={}, volumes={}, detach=True, network='lbsystem',
             env_vars={}, privileged=False, command='', labels={}):
         # If network doesn't yet exist, create it
@@ -142,5 +173,7 @@ class docker_py():
         try:
             container = client.containers.get(name)
         except docker.errors.NotFound:
+            # Returns here if the container does not exist
             return {"message": False, "status_code": 200}
+
         return {"message": str(container.status), "status_code": 200}
