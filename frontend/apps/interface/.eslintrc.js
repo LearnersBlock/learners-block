da37a728944f68bc656b6122b6c3f7827c3c7709
maggie0002 <arran.magee@icloud.com>
2021-11-24 10:18:12 +0000

    Remove disabled typescript rules

--- frontend/apps/interface/.eslintrc.js
+++ frontend/apps/interface/.eslintrc.js
@@ -96,12 +96,6 @@ module.exports = {
     quotes: ['warn', 'single', { avoidEscape: true }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
