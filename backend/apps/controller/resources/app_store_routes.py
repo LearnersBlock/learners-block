e652d6c9f0189dd513e9a88c3c0fa2928cc58d7b
maggie0002 <arran.magee@icloud.com>
2021-12-04 14:21:55 +0000

    Remove SSL requirement from App Store

--- backend/apps/controller/resources/app_store_routes.py
+++ backend/apps/controller/resources/app_store_routes.py
@@ -18,8 +18,10 @@ class appstore_get_apps(Resource):
     def get(self):
         # Fetch list of available apps from app-store repo
         try:
+            # Verify = false to avoid delay in chronyd sync
             app_list = requests.get(app_store_url,
-                                    timeout=3)
+                                    timeout=3,
+                                    verify=False)
             app_list.raise_for_status()
 
         except Exception:
