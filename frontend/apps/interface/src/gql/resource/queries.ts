ca388f7bcb9d4adca211d0c63f873ce108e546f7
maggie0002 <arran.magee@icloud.com>
2021-12-18 17:18:01 +0000

    Add multi-language support to Library

--- frontend/apps/interface/src/gql/resource/queries.ts
+++ frontend/apps/interface/src/gql/resource/queries.ts
@@ -1,61 +1,63 @@
 import gql from 'graphql-tag'
 
 export const GET_RESOURCES = gql`
-  query resources{
-    resources(filter: { download_url: {_nempty: true} }){
+query resources {
+  resources(filter: { download_url: { _nempty: true } }) {
+    id
+    name
+    description
+    languages {
       id
-      name
-      description
-      languages {
-        id,
-        languages_id {language}
+      languages_id {
+        language
       }
-      size
-      logo {
-        id
-      }
-   }
+    }
+    size
+    logo {
+      id
+    }
   }
+}
 `
 
 export const GET_RESOURCE = gql`
-  query resources($id: ID!){
-    resources_by_id(id: $id) {
+query resources($id: ID!){
+  resources_by_id(id: $id) {
+    id
+    name
+    description
+    author
+    author_website
+    sample_url
+    formats {
       id
-      name
-      description
-      author
-      author_website
-      sample_url
-      formats {
-        id,
-        formats_id {
-          format
-          }
-      }
-      download_url
-      languages {
-        id,
-        languages_id {
-          language
-          }
-      }
-      subjects {
-        id,
-        subjects_id {
-          subject
-          }
-      }
-      levels {
-        id,
-        levels_id {
-          level
-          }
-      }
-      size
-      logo {
-        id
+      formats_id {
+        key
       }
     }
+    download_url
+    languages {
+      id
+      languages_id {
+        language
+      }
+    }
+    subjects {
+      id
+      subjects_id {
+        key
+      }
+    }
+    levels {
+      id
+      levels_id {
+        key
+      }
+    }
+    size
+    logo {
+      id
+    }
   }
+}
 `
